828b1481c7fa99d2904b564b843d1624480a93d2
moibe <moi.estrello@gmail.com>
2024-10-11 22:52:16 +0000

    Push desde DoFish!

---
+++ app.py
@@ -0,0 +1,16 @@
+import gradio as gr
+
+def greet(name):
+    return f"Hello, Hola, Tervetuloa {name}."
+
+iface = gr.Interface(fn=greet, inputs="text", outputs="text").launch(root_path="/gradio-demo")
+
+
+#import gradio as gr
+#import time
+
+#def test(x):
+#	time.sleep(4)
+#	return x
+
+#gr.Interface(test, "textbox", "textbox").queue().launch(root_path="/gradio-demo")
