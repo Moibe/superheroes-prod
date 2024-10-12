f4782d0ed4857cff991bb0b2e699e79c9ff4ecae
Moibe <moi.estrello@gmail.com>
2024-10-12 03:05:07 -0600

    Back to basic

--- launcher.sh
+++ launcher.sh
@@ -1,17 +1,2 @@
 #!/bin/bash
-echo "Entrando"
-which python
-echo "Voy a ejecutar la activación del venv"
-source venv/bin/activate 
-echo "venv activado"
-echo "Haré which python otra vez"
-which python
-echo "voy a hacer ls"
-ls
-echo "ls realizado"
-echo "voy a hacer el deploy"
-python app.py 
-echo "deploy en background hecho"
-echo "finalmente voy hacia la salida"
-exit
-echo "salida realizada"
+source venv/bin/activate && python app.py \& && exit
