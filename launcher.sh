e6ad235339388375977b5d823a8eca15dcd439d2
Moibe <moi.estrello@gmail.com>
2024-10-12 02:01:22 -0600

    Echoed

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+echo "Entrando"
+echo "Voy a ejecutar la activación del venv"
 source venv/bin/activate 
+echo "venv activado"
+echo "voy a hacer ls"
 ls
+echo "ls realizado"
+echo "voy a hacer el deploy"
 python app.py \& 
+echo "deploy en background hecho"
+echo "finalmente voy hacia la salida"
 exit
+echo "salida realizada"
