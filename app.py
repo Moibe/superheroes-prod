1b648368b6ea03e2119b6e2d8a6f265bdf1105e2
Moibe <moi.estrello@gmail.com>
2024-10-13 16:52:49 -0600

    Demo Name = Demo

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import gradio as gr
 def greet(name):
     return f"Hello, Hola, Tervetuloa, Cheers, Danke {name}."
 
-gr.Interface(fn=greet, inputs="text", outputs="text").launch(root_path="/gradio-demo")
+gr.Interface(fn=greet, inputs="text", outputs="text").launch(demo_name="demo", root_path="/gradio-demo")
 
 
 #import gradio as gr
