f672a563d0107c350234ae741bf2017c8b9a4eeb
Moibe <moi_estrello@hotmail.com>
2024-10-14 11:04:45 -0600

    Reestructurar Observer

--- app.py
+++ app.py
@@ -3,15 +3,30 @@ import gradio as gr
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
+def crearObserver():
+    print("Inicio del programa...")
+    observer = Observer()
+    observer.schedule(MyHandler(), path='hola.py')
+    observer.start()
+    print("Observador iniciado, es este:", observer) 
+
+    return observer
+
+
+def iniciar():    
+    
+    print("Lanzando bloque.")
+    demo.launch(root_path="/gradio-demo")
+
 # Watch for changes in your Python file
 class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
         if event.src_path == 'hola.py':
             print("Creo que llegué aquí porque hubo un cambio1.")
             print("Y ésto es el observer:")
-            print(observer)
-            # demo.close()
-            # demo.launch(root_path="/gradio-demo")
+            #print(observer)
+            demo.close()
+            demo.launch(root_path="/gradio-demo")
             print("Detecté un cambio...")
 
 def greet(name):
@@ -25,14 +40,8 @@ with gr.Blocks() as demo:
     greet_btn.click(fn=greet, inputs=name, outputs=output, api_name="greet")
 
 try:
-    print("Inicio del programa...")
-    observer = Observer()
-    observer.schedule(MyHandler(), path='hola.py')
-    observer.start()
-    print("Observador iniciado, es este:", observer)
-    
-    print("Lanzando bloque.")
-    demo.launch(root_path="/gradio-demo")
+    observer = crearObserver() 
+    iniciar()
     
     while True:
         print("Durmiendo 5 segundos...")
