ce6cd4fa5481f68b3578554551717505dfa45fc9
Moibe <moi.estrello@gmail.com>
2024-10-13 21:39:37 -0600

    New Greet

--- app.py
+++ app.py
@@ -3,10 +3,18 @@ import gradio as gr
 def greet(name):
     return f"Hello, Hola, Tervetuloa, Cheers, Danke {name}."
 
-try:
-    demo = gr.Interface(fn=greet, inputs="text", outputs="text").launch(root_path="/gradio-demo")
-except:
-    raise gr.Error("ERROR 182")
+with gr.Blocks() as demo:
+    name = gr.Textbox(label="Name")
+    output = gr.Textbox(label="Output Box")
+    greet_btn = gr.Button("Greet")
+    greet_btn.click(fn=greet, inputs=name, outputs=output, api_name="greet")
+
+demo.launch()
+
+# try:
+#     demo = gr.Interface(fn=greet, inputs="text", outputs="text").launch(root_path="/gradio-demo")
+# except:
+#     raise gr.Error("ERROR 182")
     
 #import gradio as gr
 #import time
