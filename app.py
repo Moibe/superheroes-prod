c1bd2bbddd99f33aa141e51bacfb38d0867aed91
Moibe <moi.estrello@gmail.com>
2024-10-13 23:49:57 -0600

    Verbose Watchdog

--- app.py
+++ app.py
@@ -8,20 +8,6 @@ def slow_echo(message, history):
         time.sleep(0.05)
         yield "You typed: " + message[: i + 1]
 
-def greet(name):
-    print("Normal logging...")
-    return f"Hello, Hola, Tervetuloa, Danke, Spasiva {name}."
-
-with gr.Blocks() as demo:
-    name = gr.Textbox(label="Name")
-    output = gr.Textbox(label="Output Box")
-    greet_btn = gr.Button("Greet")
-    greet_btn.click(fn=greet, inputs=name, outputs=output, api_name="greet")
-
-demo.launch(root_path="/gradio-demo")
-
-print("Demo lanzado, llegamos a éste punto??")
-
 # Watch for changes in your Python file
 class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
@@ -30,9 +16,25 @@ class MyHandler(FileSystemEventHandler):
             demo.close()
             demo.launch()
 
+def greet(name):
+    print("Normal logging...")
+    return f"Hello, Hola, Tervetuloa, Danke, Spasiva {name}."
+
+print("Inicio del programa...")
 observer = Observer()
 observer.schedule(MyHandler(), path='app.py')
 observer.start()
+print("Observador iniciado")
+
+with gr.Blocks() as demo:
+    name = gr.Textbox(label="Name")
+    output = gr.Textbox(label="Output Box")
+    greet_btn = gr.Button("Greet")
+    greet_btn.click(fn=greet, inputs=name, outputs=output, api_name="greet")
+
+print("Lanzando bloque.")
+demo.launch(root_path="/gradio-demo")
+
 
 try:
     while True:
