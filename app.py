e6772956cd583a0492c78d7b18e0aded800ca0b0
Moibe <moi.estrello@gmail.com>
2024-10-15 21:47:53 -0600

    Reprint

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def iniciar():
     demo.launch(root_path="/gradio-demo", server_port=7860)   
 
 def greet(name):
-    return f"3rd Print: {hf_token} - {name}."
+    return f"Reprint: {hf_token} - {name}."
 
 with gr.Blocks() as demo:
     name = gr.Textbox(label="Name")
