a0f0c7808f0198ce66672a708aa3b66de72c8801
Moibe <moi.estrello@gmail.com>
2024-10-15 21:00:24 -0600

    Importing OS

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import gradio as gr
 import subprocess
+import os
+
+# Para obtener el valor de una variable de entorno:
+mi_variable = os.getenv('NOMBRE_DE_LA_VARIABLE')
+
+api_key = os.getenv('API_KEY')
+print(api_key)
 
 def iniciar():    
     print("Lanzando bloque.")
     demo.launch(root_path="/gradio-demo", server_port=7860)   
 
 def greet(name):
-    return f"3rd Print: {name}."
+    return f"3rd Print: {api_key} - {name}."
 
 with gr.Blocks() as demo:
     name = gr.Textbox(label="Name")
