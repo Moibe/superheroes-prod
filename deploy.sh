57d86c46bc67966ce83888947f46ca39cfe5cb16
moibe <moi.estrello@gmail.com>
2024-10-29 08:47:22 +0000

    Include deploy

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+pid=$(lsof -i :7860 | awk 'NR==2 {print $2}')
+kill $pid
+
+sleep 5
+
+timestamp=$(date +"%d-%m-%Y %H:%M:%S")
+echo "Proceso eliminado: $pid @ $timestamp"
+
+cd
+cd code/gradio-standalone-do/
+source venv/bin/activate
+python app.py
