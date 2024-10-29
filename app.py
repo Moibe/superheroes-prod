1c021b93614853079ab442373eb2621033aeb514
Moibe <moi.estrello@gmail.com>
2024-10-28 21:07:50 -0600

    Import llave

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import gradio as gr
 import subprocess
 import os
+import llave
 
 hf_token = os.getenv('HF_TOKEN')
 print(hf_token)
+
+parametro = llave.HF_TOKEN
+print(f"El parámetro obtenido es: {HF_TOKEN}")
 
 def iniciar():    
     print("Lanzando bloque.")
