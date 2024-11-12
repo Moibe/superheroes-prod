7400e29a183c5f2253ff6f4b97793c378d766dc0
Moibe <moi_estrello@hotmail.com>
2024-11-12 02:07:45 -0600

    Reorder perform

--- app.py
+++ app.py
@@ -1,52 +1,16 @@
 import sulkuPypi
-import SulkuFront
+import sulkuFront
 import autorizador
 import time
 import debit_rules
 import gradio as gr
-from funciones import mass
+import funciones
 import globales
 
 def iniciar():    
     app_path = globales.app_path
     main.launch(auth=autorizador.authenticate, root_path=app_path, server_port=7860)
     #Future: Si la app está dormida, no hay reacción de éste lado para avisar que está dormida.
-
-#Función principal
-def perform(input1, input2, request: gr.Request):  
-
-    #Future: Maneja una excepción para el concurrent.futures._base.CancelledError
-
-    print("Dentro de perform, request.username es: ", request.username)
-    print("Estoy por pedir la autorización, y gr.State.tokens es:  ", gr.State.tokens)  
-
-    #Future: Que no se vea el resultado anterior al cargar el nuevo resultado!         
-
-    #Importante: El uso de gr.State.tokens lo dejo en duda porque al parecer es compartido por la app para todos los usuarios!
-    #Otra opción es usar una variable, para evitar ir hasta el servidor. 
-    #La opción segura es sacarla con la API cada vez, finalmente checa el tiempo para ver si en verdad se pierde mucho.
-    tokens = sulkuPypi.getTokens(sulkuPypi.encripta(request.username).decode("utf-8"))
-    print("184: Ahora si, los tokens obtenidos por la vía larga son: ", tokens)
-
-    #1: Reglas sobre autorización si se tiene el crédito suficiente.
-    autorizacion = sulkuPypi.authorize(tokens, 'picswap')
-    if autorizacion is True:
-        #IMPORTANTE: EJECUCIÓN DE LA APP EXTERNA: mass siempre será la aplicación externa que consultamos via API.   
-        resultado = mass(input1,input2)
-    else:
-        info_window, resultado, html_credits = SulkuFront.noCredit(request.username)
-        return resultado, info_window, html_credits, btn_buy
-    
-    #**SE EJECUTA EL LLAMADO Y OFRECE UN RESULTADO.**
-    
-    #2: ¿El resultado es debitable?
-    if debit_rules.debita(resultado) == True:
-        html_credits, info_window = SulkuFront.presentacionFinal(request.username, "debita")
-    else:
-        html_credits, info_window = SulkuFront.presentacionFinal(request.username, "no debita") 
-            
-    #Lo que se le regresa oficialmente al entorno.
-    return resultado, info_window, html_credits, btn_buy
 
 #INTERFAZ
 
@@ -62,23 +26,14 @@ html_credits = gr.HTML(visible=True)
 lbl_console = gr.Label(label="AI Terminal Messages", value="AI Engine ready...", container=True)
 btn_buy = gr.Button("Get Credits", visible=True, size='lg')
 
-#ESPACIO MOMENTANEO PARA JS
-js = """
-    <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="Moibe" data-color="#FFDD00" data-emoji=""  data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" >
-    HOLA
-     var gradioContainer = document.querySelector('.gradio-container');
-    gradioContainer.insertBefore(container, gradioContainer.firstChild);
-    </script>
-    """
-
 with gr.Blocks(theme=gr.themes.Base(), css="footer {visibility: True}") as main:
    
     #Cargado en Load: Función, input, output
-    main.load(SulkuFront.display_tokens, None, html_credits) 
+    main.load(sulkuFront.display_tokens, None, html_credits) 
    
     with gr.Row():
         demo = gr.Interface(
-            fn=perform,
+            fn=funciones.perform,
             inputs=[source_image, destination_image], 
             outputs=[result_image, lbl_console, html_credits, btn_buy], 
             allow_flagging='never'
--- funciones.py
+++ funciones.py
@@ -2,9 +2,46 @@ import time
 import bridges
 import globales
 import gradio_client
+import sulkuFront
+import sulkuPypi
+import gradio as gr
+import debit_rules
 
 abrazo = bridges.hug
+btn_buy = gr.Button("Get Credits", visible=True, size='lg')
 
+#PERFORM es la app INTERNA que llamará a la app externa.
+def perform(input1, input2, request: gr.Request):
+    #Future: Maneja una excepción para el concurrent.futures._base.CancelledError
+    #Future: Que no se vea el resultado anterior al cargar el nuevo resultado!         
+
+    #Importante: El uso de gr.State.tokens lo dejo en duda porque al parecer es compartido por la app para todos los usuarios!
+    #Otra opción es usar una variable, para evitar ir hasta el servidor. 
+    #La opción segura es sacarla con la API cada vez, finalmente checa el tiempo para ver si en verdad se pierde mucho.
+    tokens = sulkuPypi.getTokens(sulkuPypi.encripta(request.username).decode("utf-8"))
+    print("Ahora si, los tokens obtenidos por la vía larga son: ", tokens)
+
+    #1: Reglas sobre autorización si se tiene el crédito suficiente.
+    autorizacion = sulkuPypi.authorize(tokens, 'picswap')
+    if autorizacion is True:
+        #IMPORTANTE: EJECUCIÓN DE LA APP EXTERNA: mass siempre será la aplicación externa que consultamos via API.   
+        resultado = mass(input1,input2)
+    else:
+        info_window, resultado, html_credits = sulkuFront.noCredit(request.username)
+        return resultado, info_window, html_credits, btn_buy
+    
+    #**SE EJECUTA EL LLAMADO Y OFRECE UN RESULTADO.**
+    
+    #2: ¿El resultado es debitable?
+    if debit_rules.debita(resultado) == True:
+        html_credits, info_window = sulkuFront.presentacionFinal(request.username, "debita")
+    else:
+        html_credits, info_window = sulkuFront.presentacionFinal(request.username, "no debita") 
+            
+    #Lo que se le regresa oficialmente al entorno.
+    return resultado, info_window, html_credits, btn_buy
+
+#MASS es la que ejecuta la aplicación EXTERNA
 def mass(input1, input2): 
 
     imagenSource = gradio_client.handle_file(input1) 
