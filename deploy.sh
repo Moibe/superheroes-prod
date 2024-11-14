661336111d75ce5111b8827ae110777c9cd05667
Moibe <moi.estrello@gmail.com>
2024-11-14 16:22:11 -0600

    Big change deploy just checks living

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 pid=$(lsof -i :7861 | awk 'NR==2 {print $2}')
 if [ -z "$pid" ]; then
-  echo "La variable pid está vacía. No se encontró ningún proceso escuchando en el puerto 7861."
+  echo "La variable pid está vacía. No se encontró ningún proceso escuchando en el puerto 7861. Entonces si reiniciaré para que exista."
+  cd
+  cd code/gradio-standalone-do/
+  source venv/bin/activate
+  python app.py &
+  PID=$(pgrep -f "python app.py")
+  timestamp2=$(date +"%d-%m-%Y %H:%M:%S")
+  echo "Proceso reiniciado: $PID @ $timestamp2"
 else
-  echo "El PID del proceso es: $pid"
+  echo "El PID del proceso es: $pid o sea que hay un proceso corriendo, no hay necesidad de matarlo ni hacer nada más que avisar: ALIVE."
 fi
 
-kill $pid
-
-sleep 5
-
-timestamp=$(date +"%d-%m-%Y %H:%M:%S")
-echo "Proceso eliminado: $pid @ $timestamp"
-
-cd
-cd code/gradio-standalone-do/
-source venv/bin/activate
-python app.py &
-PID=$(pgrep -f "python app.py")
-timestamp2=$(date +"%d-%m-%Y %H:%M:%S")
-echo "Proceso reiniciado: $PID @ $timestamp2"
+# Pasar esto a un nuevo deply que matará y arrancará pero solo ante cambios.
+# kill $pid
+#timestamp=$(date +"%d-%m-%Y %H:%M:%S")
+#echo "Proceso eliminado: $pid @ $timestamp"
