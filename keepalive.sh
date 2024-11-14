a4d955ce96c5f2831053cf685f339cba51cd2790
Moibe <moi.estrello@gmail.com>
2024-11-14 17:58:30 -0600

    KEEPALIVE

--- keepalive.sh
+++ keepalive.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+timestamp=$(date +"%d-%m-%Y %H:%M:%S")
 pid=$(lsof -i :7861 | awk 'NR==2 {print $2}')
 if [ -z "$pid" ]; then
   echo "La variable pid está vacía. No se encontró ningún proceso escuchando en el puerto 7861. Entonces si reiniciaré para que exista."
@@ -10,10 +11,9 @@ if [ -z "$pid" ]; then
   timestamp2=$(date +"%d-%m-%Y %H:%M:%S")
   echo "Proceso reiniciado: $PID @ $timestamp2"
 else
-  echo "El PID del proceso es: $pid o sea que hay un proceso corriendo, no hay necesidad de matarlo ni hacer nada más que avisar: ALIVE."
+  echo "El PID del proceso es: $pid o sea que hay un proceso corriendo, no hay necesidad de matarlo ni hacer nada más que avisar: ALIVE. $timestamp"
 fi
 
 # Pasar esto a un nuevo deply que matará y arrancará pero solo ante cambios.
 # kill $pid
-#timestamp=$(date +"%d-%m-%Y %H:%M:%S")
 #echo "Proceso eliminado: $pid @ $timestamp"
