d4aa80d0ac1c4862ce2c82505a77662f48dede3d
Moibe <moi.estrello@gmail.com>
2024-11-14 18:28:44 -0600

    Keepalive Perfeccionado

--- keepalive.sh
+++ keepalive.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-timestamp=$(date +"%d-%m-%Y %H:%M:%S")
 pid=$(lsof -i :7861 | awk 'NR==2 {print $2}')
+#Revisar si la búsqueda del proceso está vacía.
 if [ -z "$pid" ]; then
-  echo "La variable pid está vacía. No se encontró ningún proceso escuchando en el puerto 7861. Entonces si reiniciaré para que exista."
+  #Si está vacía entonces:
+  timestamp1=$(date +"%d-%m-%Y %H:%M:%S")
+  echo "$timestamp1 - ATENCIÓN: No se encontró ningún proceso escuchando en el puerto 7861. Reactivando aplicación."
+  #Reiniciando proceso
   cd
   cd code/gradio-standalone-do/
   source venv/bin/activate
   python app.py &
   PID=$(pgrep -f "python app.py")
   timestamp2=$(date +"%d-%m-%Y %H:%M:%S")
-  echo "Proceso reiniciado: $PID @ $timestamp2"
+  echo "$timestamp2 - READY: Proceso reiniciado con id $PID. "
 else
-  echo "El PID del proceso es: $pid o sea que hay un proceso corriendo, no hay necesidad de matarlo ni hacer nada más que avisar: ALIVE. $timestamp"
+  timestamp3=$(date +"%d-%m-%Y %H:%M:%S")
+  echo "timestamp3 - ALIVE: Proceso $pid arriba y funcionando."
 fi
