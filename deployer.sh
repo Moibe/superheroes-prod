4e65c55e7d53fa4a24057a035f9aa944182021ff
Moibe <moi.estrello@gmail.com>
2024-11-14 18:06:36 -0600

    deployer.sh

---
+++ deployer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+timestamp=$(date +"%d-%m-%Y %H:%M:%S")
+pid=$(lsof -i :7861 | awk 'NR==2 {print $2}')
+kill $pid
+echo "Proceso eliminado: $pid @ $timestamp"
+
+sleep 5
+
+cd
+cd code/gradio-standalone-do/
+source venv/bin/activate
+python app.py &
+PID=$(pgrep -f "python app.py")
+timestamp2=$(date +"%d-%m-%Y %H:%M:%S")
+echo "Proceso reiniciado: $PID @ $timestamp2"
