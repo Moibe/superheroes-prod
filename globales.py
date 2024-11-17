fe33e7d64edceb2a11c2afcc46a679a7d9b412d4
Moibe <moi_estrello@hotmail.com>
2024-11-16 19:33:29 -0600

    Ordenamiento de funciones en sulkuFront

--- globales.py
+++ globales.py
@@ -2,7 +2,7 @@ import gradio as gr
 import tools
 
 #MAIN
-version = "1.0.0"
+version = "1.0.1"
 env = "dev"
 aplicacion = "astroblend-dev"
 api = "Moibe/image-blend"
--- sulkuFront.py
+++ sulkuFront.py
@@ -3,6 +3,28 @@ import globales
 import sulkuPypi
 import gradio as gr
 
+def display_tokens(request: gr.Request):
+   
+    novelty = sulkuPypi.getNovelty(sulkuPypi.encripta(request.username).decode("utf-8"), globales.aplicacion)
+    
+    if novelty == "new_user": 
+        display = gr.Textbox(visible=False)
+    else: 
+        tokens = sulkuPypi.getTokens(sulkuPypi.encripta(request.username).decode("utf-8"), globales.env)
+        display = visualizar_creditos(tokens, request.username)      
+    
+    return display
+
+def visualizar_creditos(nuevos_creditos, usuario):
+
+    html_credits = f"""
+    <div>
+    <div style="text-align: left;">👤<b>Username: </b> {usuario}</div><div style="text-align: right;">💶<b>Credits Available: </b> {nuevos_creditos}</div>
+    </div>
+                    """    
+     
+    return html_credits
+
 #Controla lo que se depliega en el frontend y que tiene que ver con llamados a Sulku.
 def noCredit(usuario):
     info_window = "Out of credits..."
@@ -26,25 +48,3 @@ def presentacionFinal(usuario, accion):
     html_credits = visualizar_creditos(tokens, usuario)       
     
     return html_credits, info_window
-
-def display_tokens(request: gr.Request):
-   
-    novelty = sulkuPypi.getNovelty(sulkuPypi.encripta(request.username).decode("utf-8"), globales.aplicacion)
-    
-    if novelty == "new_user": 
-        display = gr.Textbox(visible=False)
-    else: 
-        tokens = sulkuPypi.getTokens(sulkuPypi.encripta(request.username).decode("utf-8"), globales.env)
-        display = visualizar_creditos(tokens, request.username)      
-    
-    return display
-
-def visualizar_creditos(nuevos_creditos, usuario):
-
-    html_credits = f"""
-    <div>
-    <div style="text-align: left;">👤<b>Username: </b> {usuario}</div><div style="text-align: right;">💶<b>Credits Available: </b> {nuevos_creditos}</div>
-    </div>
-                    """    
-     
-    return html_credits
