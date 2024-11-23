21b3cfce70d381168e203a199f4844a2c14befd8
Moibe <moi_estrello@hotmail.com>
2024-11-22 21:54:45 -0600

    Source Result

--- globales.py
+++ globales.py
@@ -1,7 +1,7 @@
 import gradio as gr
 
 #MAIN
-version = "4.4.11"
+version = "4.4.12"
 env = "dev"
 aplicacion = "superheroes-dev" #como se llama en tu repo y tu dominio.
 #api = "Moibe/splashmix"
--- sets.py
+++ sets.py
@@ -3,6 +3,6 @@ import gradio as gr
 configuraciones = {
     "splashmix": {
         "input1": gr.Image(label="Source", type="filepath"),
-        "result": gr.Image(label="Source", type="filepath"),
+        "result": gr.Image(label="Result", type="filepath"),
     }
 }
