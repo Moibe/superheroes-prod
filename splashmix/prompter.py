2bdaa696797c4709b4b5fdc7511ab4c8ad744706
Moibe <moi_estrello@hotmail.com>
2025-01-28 19:24:24 -0600

    Cartriges Ready

--- splashmix/prompter.py
+++ splashmix/prompter.py
@@ -1,63 +1,11 @@
-import globales
-from splashmix.objetosCreacion import Hotgirl, Superhero
 import splashmix.configuracion
-#POR EL MOMENTO SOLO SE ESTÁ USANDO promteador, los demás vienen de splashmix-batcher.
 
-def obten(dataframe, indice, atributo):        
-    valor = dataframe.loc[indice[0], atributo]  
-    return valor
+#Ésto requiere que cada tipo de creación que hagas tenga un prompt único y se pone aquí de forma muy manual.
+#El prompt en forma de variable incluirá los atributos para ese objeto, así mismo si quieres más, trabaja en la parte...
+#...donde se define ése objeto. 
 
-def obtenAtributosObjeto(sample_objeto): 
-    atributos = []
-    for nombre_atributo in dir(sample_objeto):
-        if not nombre_atributo.startswith("__"):
-            atributos.append(nombre_atributo)
-    return atributos
-
-
-def creaContenedorTemplate(dataframe, indice, objeto):    
-    #Prompt es la frase que ordena a los atributos que ya tenemos.
-    if objeto == "Superhero":
-        sample_objeto = Superhero()
-    else:
-        sample_objeto = Hotgirl()    
-    #Obtiene los atributos del tipo de objeto, por ejemplo style y heroe.
-    atributos = obtenAtributosObjeto(sample_objeto)
-    contenedor = {}
-
-    for atributo in atributos:        
-        contenedor[atributo] = obten(dataframe, indice, atributo) if isinstance(obten(dataframe, indice, atributo), str) else ""
-    #Al final agrega el shot porque siempre lo traerá.
-    contenedor['shot'] = obten(dataframe, indice, 'Shot')     
-    return contenedor
-
-def creaPrompt(contenedor, creacion):
-    #FUTURE: Detectar atributos dinámicamente.
-    if creacion == "Superhero":         
-        style = contenedor['style'] #if isinstance(contenedor.get('style'), str) else ""
-        subject = contenedor['subject'] #if isinstance(contenedor.get('style'), str) else ""     
-        #PROMPT PARA HEROE
-        prompt = f"A {style} of a superhero like {subject} " #agregar otros atributos random aquí posteriormente.  
-    else:            
-        #Importante El if instance es porque si viene como float nan, lo cambio a texto que sea vacío.
-        #Porque si no me parece que deja la palabra nan o lo manifiesta como float, tendríamos que probar.
-        style = contenedor['style'] #if isinstance(contenedor.get('style'), str) else ""
-        adjective = contenedor['adjective'] #if isinstance(contenedor.get('adjective'), str) else ""
-        boobs = contenedor['boobs'] #if isinstance(contenedor.get('style'), str) else ""
-        complemento = contenedor['complemento'] #if isinstance(contenedor.get('style'), str) else ""
-        hair_style = contenedor['hair_style'] #if isinstance(contenedor.get('style'), str) else ""
-        place = contenedor['place'] #if isinstance(contenedor.get('place'), str) else ""
-        situacion = contenedor['situacion'] #if isinstance(contenedor.get('style'), str) else ""
-        subject = contenedor['subject'] #if isinstance(contenedor.get('style'), str) else ""
-        type_girl = contenedor['type_girl'] #if isinstance(contenedor.get('style'), str) else ""
-        wardrobe_top = contenedor['wardrobe_top'] #if isinstance(contenedor.get('style'), str) else ""
-        wardrobe_accesories = contenedor['wardrobe_accesories'] #if isinstance(contenedor.get('style'), str) else ""
-        wardrobe_bottom = contenedor['wardrobe_bottom'] #if isinstance(contenedor.get('style'), str) else ""
-        wardrobe_shoes = contenedor['wardrobe_shoes'] #if isinstance(contenedor.get('style'), str) else ""
-        
-        prompt = f"""A {style} of a {adjective} {type_girl} {subject} with {boobs} and {hair_style} wearing {wardrobe_top}, 
-                {wardrobe_accesories}, {wardrobe_bottom}, {wardrobe_shoes}, {situacion} at {place} {complemento}"""   
-    return prompt
+#IMPORTANTE: Nombre diccionario dice exactamente que databank usa el objeto en cuestión. 
+#Los databanks se alojan en la carpeta data.
 
 def prompteador(objeto, nombre_diccionario):
     datos = getattr(splashmix.configuracion, nombre_diccionario)
--- splashmix/static_databanks.py
+++ splashmix/static_databanks.py
@@ -4,6 +4,10 @@ general_data = 'data_gral'
 
 #Particulares
 #Se obtiene todo el resto de las características que no son estilo.
+
+#Future: Puedes agregar un if aquí para que el set seleccionado dependa por ejemplo del usuario, en donde una misma...
+#...app como superheroes-dev sirva con dos fdatabanks distintos por ejemplo a LeoSell y a RevgenLabs.
+
 Superhero = 'data_superhero' 
 Superheroine = 'data_superheroine'
 Hotgirl = 'data_hotgirl'
