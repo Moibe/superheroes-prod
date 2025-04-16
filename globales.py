7dbe7335cd5711b3e055670530a0e1c2d69ccdf3
Moibe <moi.estrello@gmail.com>
2025-04-15 21:08:30 -0600

    7800 en KeepAlive

--- globales.py
+++ globales.py
@@ -4,7 +4,7 @@ app_path = "/superheroes-prod"
 server_port=7800
 
 #MAIN
-version = "1.3.2" #Ports Ready Original.
+version = "1.3.3" #7800 en Keepalive.
 
 nombre_diccionario = "datos_superheroe"
 
