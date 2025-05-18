47f19134ecf407185ddf0df61c50ec958318101a
Moibe <moi.estrello@gmail.com>
2025-05-18 00:08:12 -0600

    3 Scripts

--- firehead.py
+++ firehead.py
@@ -3,6 +3,6 @@ head = """
     <script src="https://www.gstatic.com/firebasejs/10.7.0/firebase-auth-compat.js"></script>
     <script src="https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore-compat.js"></script>  
     <script>
-    console.log("Hola estoy en head")    
+    console.log("Hola estoy en HEAD.js")    
     </script>     
 """
