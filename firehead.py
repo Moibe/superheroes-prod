e3f9f225f4e6310bc23602a20510f331b6bbed2c
Moibe <moi.estrello@gmail.com>
2025-05-18 00:35:51 -0600

    Head timeout

--- firehead.py
+++ firehead.py
@@ -3,6 +3,8 @@ head = """
     <script src="https://www.gstatic.com/firebasejs/10.7.0/firebase-auth-compat.js"></script>
     <script src="https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore-compat.js"></script>  
     <script>
-    console.log("Hola estoy en HEAD.js")    
-    </script>     
+  setTimeout(() => {
+    console.log("Hola estoy en HEAD.js");
+  }, 15000); // 15000 milisegundos = 15 segundos
+</script>    
 """
