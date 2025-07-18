2c47a55582cb223bca891354e14507d23c24e118
Moibe <moi.estrello@gmail.com>
2025-07-17 23:27:12 -0600

    Hardcode new user

--- sulkuFront.py
+++ sulkuFront.py
@@ -34,7 +34,7 @@ def precarga(uid):
 
     
     try: 
-        #uid = '5X8Hhd70uRclG1qfSJVj2zm211Q2182' 
+        uid = 'gC79Iw38JSV7wpSJt4wWZwYFRgt1' 
         print("Estoy en precarga y el usuario recibido es: ", uid)
         email, displayName = fireWhale.obtenDatosUIDFirebase(uid)
         print(f"Email: {email}, displayName: {displayName}.")
