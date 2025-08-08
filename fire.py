1f27594353ff961121e9152a7ccc0d8319118d59
Moibe <moi.estrello@gmail.com>
2025-08-08 02:24:33 -0600

    From outside fire

--- fire.py
+++ fire.py
@@ -5,7 +5,7 @@ function normal(a) {{
 
     console.log("Entré a fire.js @ BLOCKS y esto es a: ", a)
     resultado = localStorage.getItem('uid');
-    console.log("Éste es el usuario que obtuvo fuego cuando hay user auth desde afuera de fire: ", resultado)
+    console.log("1Éste es el usuario que obtuvo fuego cuando hay user auth desde afuera de fire: ", resultado)
     {fireconfig.firebase_config}
     firebase.initializeApp(firebaseConfig);
     const provider = new firebase.auth.GoogleAuthProvider();
