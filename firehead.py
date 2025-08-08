2dae98e1522e22b5580c47d801353f6dc7460ca4
Moibe <moi.estrello@gmail.com>
2025-08-08 13:53:28 -0600

    Head marca reload en false

--- firehead.py
+++ firehead.py
@@ -3,6 +3,7 @@ head = """
     <script src="https://www.gstatic.com/firebasejs/10.7.0/firebase-auth-compat.js"></script>
     <script src="https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore-compat.js"></script>  
     <script>
-    console.log("Hola estoy en HEAD.js de firehead.py @ BLOCKS")    
+    console.log("Hola estoy en HEAD.js de firehead.py @ BLOCKS")
+    localStorage.setItem('reloaded', 'false')    
     </script>     
 """
