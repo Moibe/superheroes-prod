59959415784c52139784c39ea1d979bb81671ed8
Moibe <moi.estrello@gmail.com>
2025-08-24 04:04:53 -0600

    Keepalive Dev

--- keepalive_dev.sh
+++ keepalive_dev.sh
@@ -7,7 +7,7 @@ if [ -z "$pid" ]; then
   echo "$timestamp1 - ATENCIÓN: No se encontró ningún proceso escuchando en el puerto 7800. Reactivando aplicación."
   #Reiniciando proceso
   cd
-  cd code/superheroes-prod/
+  cd code/superheroes-dev/
   source venv/bin/activate
   python app.py &
   nuevo_proceso=$(pgrep -f "python app.py")
